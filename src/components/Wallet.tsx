4d223bf5038c7d1f87b7f4ba7bbc23326ada622b
Gabriel Kuettel <gabrielkuettel@gmail.com>
2022-10-19 17:16:12 -0500

    show transaction status

--- src/components/Wallet.tsx
+++ src/components/Wallet.tsx
@@ -10,6 +10,8 @@ import algosdk from "algosdk";
 const algodClient = new algosdk.Algodv2(NODE_TOKEN, NODE_SERVER, NODE_PORT);
 
 export default function Wallet() {
+  const [status, setStatus] = React.useState<string | null>();
+
   const { activeAccount, signTransactions, sendTransactions } = useWallet();
 
   const sendTransaction = async (
@@ -17,6 +19,8 @@ export default function Wallet() {
     to?: string,
     amount?: number
   ) => {
+    setStatus(null);
+
     if (!from || !to || !amount) {
       throw new Error("Missing transaction params.");
     }
@@ -31,9 +35,16 @@ export default function Wallet() {
     });
 
     const encodedTransaction = algosdk.encodeUnsignedTransaction(transaction);
+
+    setStatus("Waiting to be signed...");
+
     const signedTransactions = await signTransactions([encodedTransaction]);
 
+    setStatus("Sending...");
+
     const { id } = await sendTransactions(signedTransactions);
+
+    setStatus("Success!");
 
     console.log("Successfully sent transaction. Transaction ID: ", id);
   };
@@ -44,20 +55,15 @@ export default function Wallet() {
 
   return (
     <div>
-      {
-        <button
-          onClick={() =>
-            sendTransaction(
-              activeAccount?.address,
-              activeAccount?.address,
-              1000
-            )
-          }
-          className="button"
-        >
-          Sign and send transactions
-        </button>
-      }
+      <button
+        onClick={() =>
+          sendTransaction(activeAccount?.address, activeAccount?.address, 1000)
+        }
+        className="button"
+      >
+        Sign and send transactions
+      </button>
+      {status && <h5>{status}</h5>}
     </div>
   );
 }
